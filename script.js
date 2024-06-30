901279a22e37561aef1760d84123c6184d90054d
Heera Ballabh <heeraballabh@Heeras-MacBook-Pro.local>
2024-06-30 23:53:10 +0530

    added form for adding msg

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-document.getElementById("contact-form").addEventListener("submit", function(event) {
+// document.getElementById("contact-form").addEventListener("submit", function(event) {
+//     event.preventDefault();
+//     alert("Thank you for your message. We will get back to you soon!");
+//     this.reset();
+// });
+
+
+function sendWhatsAppMessage(event) {
     event.preventDefault();
-    alert("Thank you for your message. We will get back to you soon!");
-    this.reset();
-});
+
+    const name = document.getElementById('name').value;
+    const mobile = document.getElementById('mobile').value;
+    const message = document.getElementById('message').value;
+
+    const whatsappNumber = '+918088676341';  // Replace with the actual WhatsApp number
+    const whatsappMessage = `Name: ${name}%0AMobile: ${mobile}%0AMessage: ${message}`;
+    const whatsappURL = `https://wa.me/${whatsappNumber}?text=${whatsappMessage}`;
+
+    window.open(whatsappURL, '_blank');
+}
